38369c882602d4b4e05f6c609d53f5266be9abe9
Rachit Nigam <rachit.nigam12@gmail.com>
2023-03-05 13:31:14 -0800

    more aggressive configuration script

--- scripts/configure-fud.sh
+++ scripts/configure-fud.sh
@@ -5,6 +5,35 @@ set -euf -o pipefail
 # Update the submodules
 git submodule update --init --recursive
 
+# Install fud
+## Check if flit is installed
+if ! command -v flit &> /dev/null
+then
+    >&2 echo "Flit is not installed. Installing..."
+    python -m pip install flit
+fi
+## Check if fud is installed
+if ! command -v fud &> /dev/null
+then
+    >&2 echo "Fud is not installed. Installing..."
+    cd calyx/fud && flit install -s && cd ../..
+fi
+## Check if jq is installed
+if ! command -v jq &> /dev/null
+then
+    >&2 echo "jq is not installed. Please install it before continuing..."
+    exit 1
+fi
+## Check if iverilog is installed
+if ! command -v iverilog &> /dev/null
+then
+    >&2 echo "iverilog is not installed. Please install it before continuing..."
+    exit 1
+fi
+
+# Install cocotb and dependencies
+python -m pip install cocotb pytest
+
 # Build the tools
 cd calyx && cargo build && cd ..
 cd filament && cargo build && cd ..
