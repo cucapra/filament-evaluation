347c52b73a3d0aa71dd5ff0f229b144785afd15d
Rachit Nigam <rachit.nigam12@gmail.com>
2023-03-05 13:25:45 -0800

    build calyx & filament

--- scripts/configure-fud.sh
+++ scripts/configure-fud.sh
@@ -2,7 +2,9 @@
 
 set -euf -o pipefail
 
-fud c global "$(pwd)/calyx"
+cd calyx && cargo build && cd ..
+cd filament && cargo build && cd ..
+
 fud c stages.futil.exec "$(pwd)/calyx/target/debug/futil"
 fud c stages.filament.exec "$(pwd)/filament/target/debug/filament"
 fud check
