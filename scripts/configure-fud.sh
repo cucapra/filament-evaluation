310300d910cea093431406b703ead8774a5424ed
Rachit Nigam <rachit.nigam12@gmail.com>
2023-03-05 13:23:59 -0800

    pwd call

--- scripts/configure-fud.sh
+++ scripts/configure-fud.sh
@@ -2,7 +2,7 @@
 
 set -euf -o pipefail
 
-fud c global "$pwd/calyx"
-fud c stages.futil.exec "$pwd/calyx/target/debug/futil"
-fud c stages.filament.exec "$pwd/filament/target/debug/filament"
+fud c global "$(pwd)/calyx"
+fud c stages.futil.exec "$(pwd)/calyx/target/debug/futil"
+fud c stages.filament.exec "$(pwd)/filament/target/debug/filament"
 fud check
