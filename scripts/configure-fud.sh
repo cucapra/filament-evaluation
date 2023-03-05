0b3f0d71a449370c12bafc9efb3998347934ed05
Rachit Nigam <rachit.nigam12@gmail.com>
2023-03-05 13:23:22 -0800

    script to configure fud

---
+++ scripts/configure-fud.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -euf -o pipefail
+
+fud c global "$pwd/calyx"
+fud c stages.futil.exec "$pwd/calyx/target/debug/futil"
+fud c stages.filament.exec "$pwd/filament/target/debug/filament"
+fud check
