146e963f096f35065cd3f6c0290f18a49878b480
Rachit Nigam <rachit.nigam12@gmail.com>
2023-03-05 13:28:12 -0800

    update modules during configuration

--- scripts/configure-fud.sh
+++ scripts/configure-fud.sh
@@ -2,9 +2,14 @@
 
 set -euf -o pipefail
 
+# Update the submodules
+git submodule update --init --recursive
+
+# Build the tools
 cd calyx && cargo build && cd ..
 cd filament && cargo build && cd ..
 
+# Configure fud
 fud c stages.futil.exec "$(pwd)/calyx/target/debug/futil"
 fud c stages.filament.exec "$(pwd)/filament/target/debug/filament"
 fud c stages.cocotb.exec "$(pwd)/filament/target/debug/filament"
