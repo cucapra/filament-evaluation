2950cfa6980e50dc18014edcd65032c33fd250f2
Rachit Nigam <rachit.nigam12@gmail.com>
2023-03-05 13:27:03 -0800

    cocotb exec

--- scripts/configure-fud.sh
+++ scripts/configure-fud.sh
@@ -7,4 +7,5 @@ cd filament && cargo build && cd ..
 
 fud c stages.futil.exec "$(pwd)/calyx/target/debug/futil"
 fud c stages.filament.exec "$(pwd)/filament/target/debug/filament"
+fud c stages.cocotb.exec "$(pwd)/filament/target/debug/filament"
 fud check
